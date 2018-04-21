d3bb4f77633416e74a2f546c67f6e3090c7187f1
joshuaoben <olonadejoshua@gmail.com>
2018-04-21 18:44:12 +0100

    scripted form validation, and added additional styling

--- js/script.js
+++ js/script.js
@@ -3,16 +3,39 @@
 
 function Check() {
 	var name = document.getElementById('name').value,
-		date = document.getElementById('date').value
+		date = document.getElementById('date').value,
 		display_name = document.getElementById('display_name'),
 		right_comp = document.getElementById('right_comp'),
-		output = document.getElementById('output');
+		output = document.getElementById('output'),
+		error = document.getElementById('error');
 
-		output.style.opacity= "100";
-		right_comp.setAttribute("class", "right_comp_hover");
+		if (name && date) {
 
-		console.log(name, date);
-		display_name.innerHTML = name;
+			error.setAttribute("class", "no_error");
+			output.style.opacity= "100";
+			right_comp.setAttribute("class", "right_comp_hover");
+			display_name.innerHTML = name;
+		}
+
+		else {
+			error.setAttribute("class", "error");
+			error.innerHTML = "Please input both Name and Date";
+		}
+
+}
+
+function Back() {
+	var back = document.getElementById('back'),
+		right_comp = document.getElementById('right_comp'),
+		name = document.getElementById('name'),
+		date = document.getElementById('date');
+
+	output.style.opacity= "0";
+
+	right_comp.setAttribute("class", "right_comp");
+
+	name.value = "";
+	date.value = "";
 
 
 }
